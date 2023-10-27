d9abc38da900f0da2fe95115d42be3df366e7b86
yvdriel <yoranvd2001@gmail.com>
2023-10-27 17:28:26 +0200

    create base for development server and ticket purchase

--- api/index.js
+++ api/index.js
@@ -1,16 +1,21 @@
-const app = require('express')();
+const app = require("express")();
+const body = require("./body.js");
+const bodyParser = require('body-parser')
+
+const jsonParser = bodyParser.json()
+
 // const { v4 } = require('uuid');
 
-app.get('/api', (req, res) => {
+app.get("/api", (req, res) => {
   const path = `/api/item/`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end(`Hello! Go todf item: <a href="${path}">${path}</a>`);
 });
 
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
+app.post("/api/ticket/purchase", jsonParser, (req, res) => {
+    res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+    res.json(body);
 });
 
 module.exports = app;
