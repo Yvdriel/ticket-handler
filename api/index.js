555ea4f8ac0ddf54fc6dbb3bc3fd4ce0c7940d99
yvdriel <yoranvd2001@gmail.com>
2023-10-28 13:20:14 +0200

    Create base for body_mapper

--- api/index.js
+++ api/index.js
@@ -29,7 +29,7 @@ app.post("/api/ticket/purchase", jsonParser, (req, res) => {
   res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
 
   service
-    .post("/posts", body_mapper(req.params))
+    .post("/posts", body_mapper(req.body))
     .then((response) => {
       res.json(response.data);
     })
--- api/plugins/body_mapper.js
+++ api/plugins/body_mapper.js
@@ -1,6 +1,7 @@
 const body = require("../body.js");
 
 const body_mapper = (request) => {
+    console.log(request)
     return body
 }
 
